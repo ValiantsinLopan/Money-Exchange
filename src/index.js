8c483adaae57a006d50780ae6af013005e7eaeb8
Valentin Lopan <valiantsin_lopan@live.com>
2018-09-25 22:10:38 +0300

    implement makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let result = {};
+    
+    if (currency > 10000){
+       return result = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    };
+
+    let half = Math.floor(currency / 50);
+    let quarters =  Math.floor((currency - (half * 50)) / 25);
+    let dimes = Math.floor((currency - (half * 50 + quarters * 25)) / 10);
+    let nickels = Math.floor((currency - (half * 50 + quarters * 25 + dimes * 10)) / 5);
+    let pennies = currency - (half * 50 + quarters * 25 + dimes * 10 + nickels * 5);
+
+    result['H'] = half;
+    result['Q'] = quarters;
+    result['D'] = dimes;
+    result['N'] = nickels;
+    result['P'] = pennies;
+
+    Object.keys(result).forEach((key) => (result[key] <= 0) && delete result[key]);
+    return result;
 }
